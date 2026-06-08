3578e5169cacaf963abc557cc7fa8b48a7d8e7d6
aditya pradeep <adi112415@yahoo.com>
2026-06-08 12:48:25 -0400

    Adding initial homepage test

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "homepage.html"));
+});
+
+app.listen(3000);
